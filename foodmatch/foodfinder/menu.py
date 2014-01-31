8501981cc542e615bbd74631aba7d14a600b7bdd
Kevin Lin <kaikai526@gmail.com>
2014-01-30 19:12:00 -0800

    remove debug line

--- foodmatch/foodfinder/menu.py
+++ foodmatch/foodfinder/menu.py
@@ -6,5 +6,16 @@ class Menu(object):
 	def add_item(self, food):
 		self.items.append(food)
 
+	def match_name_with(self, preferences):
+		preferences = preferences.split(',')
+		preferences = [preference.replace(" ", "") for preference in preferences]
+		item_names = [item.name for item in self.items]
+		matches = []
+		for preference in preferences:
+			for i, item_name in enumerate(item_names): 
+				if preference in item_name:
+					matches.append(self.items[i]) # TODO: append food object, not name
+		return matches
+
 	def __repr__(self):
 		return self.items.__repr__()
